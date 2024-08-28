d454469f86a2b0790b982998fbb5a46770f473b2
Thanchida <thanchida.a@ku.th>
2024-08-28 17:14:25 +0700

    modify views.py using can_vote and is_published'

--- polls/views.py
+++ polls/views.py
@@ -41,6 +41,12 @@ class DetailView(generic.DetailView):
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
+    def get_only_published(self, request):
+        question = self.get_object()
+        if not question.is_published():
+            return render(request, 'polls/detail.html',
+                          {'error_message': "This question is not publish yet"})
+
 
 class ResultsView(generic.DetailView):
     """
@@ -62,6 +68,11 @@ def vote(request, question_id):
     :return: Redirect to the result page
     """
     question = get_object_or_404(Question, pk=question_id)
+
+    if not question.can_vote():
+        return render(request, 'polls/detail.html', {'question': question,
+                                                     'error_message': "Voting is not allow now"})
+
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
