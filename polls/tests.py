19afc50bfc56e0b8340e32c774ed0500cc85cbdd
Thanchida <thanchida.a@ku.th>
2024-09-01 16:20:12 +0700

    Add python-decouple

--- polls/tests.py
+++ polls/tests.py
@@ -113,7 +113,7 @@ class QuestionIndexViewTests(TestCase):
         If no questions exist, an appropriate message is displayed.
         """
         response = self.client.get(reverse('polls:index'))
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No polls are available.")
         self.assertQuerySetEqual(response.context['latest_question_list'], [])
 
