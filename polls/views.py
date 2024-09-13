2bb5ab3088013a2b50c527c6ba5427094f5ff3bb
Thanchida <thanchida.a@ku.th>
2024-09-13 22:12:40 +0700

    Update index page, Show all published question

--- polls/views.py
+++ polls/views.py
@@ -29,7 +29,7 @@ class IndexView(generic.ListView):
         """
         Return the last five published questions.
         """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
 
 
 class DetailView(generic.DetailView):
